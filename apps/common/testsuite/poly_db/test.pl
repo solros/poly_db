9e3a18631711663c6dfed00e2f4038af12d5c9d3
Silke Horn <silke@solros.de>
2014-02-02 01:41:00 +0100

    testsuite

--- apps/common/testsuite/poly_db/test.pl
+++ apps/common/testsuite/poly_db/test.pl
@@ -1,4 +1,4 @@
-compare_data( '1', poly_db({"CONE_DIM" => 4}, db=>"LatticePolytopes", collection=>"SmoothReflexive") )
+compare_data( '1', poly_db({"CONE_DIM"=>4},db=>"LatticePolytopes", collection=>"SmoothReflexive", sort_by=>{"_id"=>1}) )
 	and
-compare_data( '2', poly_db({"CONE_DIM" => 8}, db=>"LatticePolytopes", collection=>"SmoothReflexive", limit => 10) );
+compare_data( '2', poly_db({"CONE_DIM" => 8}, db=>"LatticePolytopes", collection=>"SmoothReflexive", limit=>10, sort_by=>{"_id"=>1}, skip=>20) );
 ;
