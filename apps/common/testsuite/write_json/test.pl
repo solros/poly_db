234a169f3c52163a9104e2b47421a9f9255efa34
Silke Horn <silke@solros.de>
2014-02-20 13:35:10 +0100

    testsuite: * more tests for write_json * testcases that check the type information

--- apps/common/testsuite/write_json/test.pl
+++ apps/common/testsuite/write_json/test.pl
@@ -2,4 +2,6 @@ compare_output { print write_json(load('1.poly')) } '1'
 	and
 compare_output { print write_json(load('2.poly')) } '2' 
 	and
-compare_output { print write_json(load('3.poly')) } '3' ;
+compare_output { print write_json(load('3.poly')) } '3'
+	and
+compare_output { print write_json(load('4.poly')) } '4' ;
