57012beea49b9ad519ee5d098882f7f1702289c6
Andreas Paffenholz <paffenholz@mathematik.tu-darmstadt.de>
2016-08-26 16:31:36 +0200

    testcase insert

--- apps/common/testsuite/db_insert_with_template/test.pl
+++ apps/common/testsuite/db_insert_with_template/test.pl
@@ -5,34 +5,23 @@ my $date = DateTime->now->date;
 my $name = Sys::Hostname::hostname;
 my $col_name = "test-".$name."-".$now;
 
-#set_type_info("Test", $col_name, template=>load("t.poly"));
+my $t = db_get_template("LatticePolytopes", "SmoothReflexive", template_key=>"fano");
+db_set_type_info("Test", $col_name, template=>$t->{template}, template_key=>$col_name, app=>"polytope", basic_type=>"polytope::Polytope<Rational>", id=>$col_name);
+my $s = db_get_template("Test", $col_name, template_key=>$col_name);
 
-#my $t = get_template_object("Test", $col_name);
+my $parray = load_data("1_in.pdata");
+db_insert_array($parray, "Test", $col_name, use_template=>1, template_key=>$col_name);
 
-#db_insert_pdata("1_in.pdata", "Test", $col_name, use_template=>1);
+my $count = db_count({}, db=>"Test", collection=>$col_name);
 
-#my $count = poly_db_count({}, db=>"Test", collection=>$col_name);
+my $retarray = db_query({}, db=>"Test", collection=>$col_name, sort_by=>{"_id"=>1});
 
-#my $out = poly_db({}, db=>"Test", collection=>$col_name, sort_by=>{"_id"=>1});
+foreach (@{db_ids({}, db=>"Test", collection=>$col_name)}) {
+	db_remove($_, "Test", $col_name);
+}
 
-#my $out_ok = load_data("1.pdata");
-
-#foreach (@{poly_db_ids({}, db=>"Test", collection=>$col_name)}) {
-#	db_remove($_, "Test", $col_name);
-#}
-
-#compare_object( 't_out.poly', $t, ignore=>['name'] )
-#	and
-#compare_values( 'count', 18, $count )
-#	and
-#do {
-#	for (my $i=0; $i<$out->size; ++$i) {
-#		if(!_compare_object($out_ok->[$i], $out->[$i], ignore=>['collection', 'date'])) {
-#			return 0;
-#		}
-#	}
-#	1;
-#}
-#	and
-#compare_values( 'remove', 0, poly_db_count({}, db => "Test", collection => $col_name) )
-#;
+save_data($retarray,"1_out.pdata");
+compare_values( 'count', 18, $count );
+compare_data('1',$retarray);
+compare_values( 'remove', 0, db_count({}, db => "Test", collection => $col_name) );
+#db_clean_up_test();
